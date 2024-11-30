c30aa0ee4f2adacb313b772fa00a793db98cc8d1
Justin <jw4566c@gre.ac.uk>
2024-11-30 20:52:36 +0000

    tweaking the echo a bit and created new scene for movement testing

--- COMP1870_FYIP/Assets/Scripts/PulseTrigger.cs
+++ COMP1870_FYIP/Assets/Scripts/PulseTrigger.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class PulseTrigger : MonoBehaviour
 {
     SphereCollider sphereCol;
-
     ParticleSystem ps;
-    //List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
 
     private void OnEnable()
     {
@@ -20,24 +18,9 @@ public class PulseTrigger : MonoBehaviour
         ExpandColWithPS();
     }
 
-    /*private void OnParticleTrigger()
-    {
-        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-
-        for(int i = 0; i < numEnter; i++)
-        {
-            ParticleSystem.Particle p = enter[i];
-            Debug.Log($"HIT {p}");
-        }
-    }*/
 
     void ExpandColWithPS()
     {
-        /*ParticleSystem.MainModule main = ps.main;
-        float max = main.startSize.constant;
-        float delta = ps.time / main.startLifetime.constant;
-        sphereCol.radius = max * delta * 0.5f;*/
-
         ParticleSystem.SizeOverLifetimeModule sol = ps.sizeOverLifetime;
         float delta = Mathf.Clamp01(ps.time / ps.main.startLifetime.constant);
         float currentSize = sol.size.Evaluate(delta);
@@ -49,5 +32,7 @@ public class PulseTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"HIT {other.name}");
+
     }
+
 }
