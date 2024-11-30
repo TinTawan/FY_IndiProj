e954a249df5f5001988f0faea6cd41f8aeb4ef21
Justin <jw4566c@gre.ac.uk>
2024-11-30 17:27:03 +0000

    can pulse using space bar
    sends pulse out from the camera

---
+++ COMP1870_FYIP/Assets/Scripts/SendEchoPulse.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendEchoPulse : MonoBehaviour
+{
+    [SerializeField] private GameObject pulsePrefab;
+    [SerializeField] private float pulseDuration;
+    [SerializeField] private int pulseSize;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Pulse();
+        }
+    }
+
+    void Pulse()
+    {
+        GameObject pulse = Instantiate(pulsePrefab, transform.position, Quaternion.identity);
+        ParticleSystem pulsePS = pulse.transform.GetComponentInChildren<ParticleSystem>();
+
+        pulsePS.Stop();
+
+        ParticleSystem.MainModule pulseMain = pulsePS.main;
+        pulseMain.startLifetime = pulseDuration;
+        pulseMain.startSize = pulseSize;
+
+        pulsePS.Play();
+
+
+        Destroy(pulse, pulseDuration + 0.5f);
+    }
+}
