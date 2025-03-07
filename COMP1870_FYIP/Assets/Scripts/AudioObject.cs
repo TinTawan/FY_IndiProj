7b9d54610031ea7cf9e7816b9fec03362d11d56b
Justin Ward <jw4566c@gre.ac.uk>
2025-03-07 20:49:03 +0000

    added audio manager
    audio object prefab and script set up, need to set up which sounds are needed in the manager

---
+++ COMP1870_FYIP/Assets/Scripts/AudioObject.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class AudioObject : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    AudioClip clip;
+    float vol = 1f, pitch = 1f;
+
+
+    public void StartAudio()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.volume = vol;
+        audioSource.pitch = 1f + pitch;
+        audioSource.Play();
+
+    }
+
+    public void SetClip(AudioClip inClip)
+    {
+        clip = inClip;
+    }
+    public void SetVolume(float inVol)
+    {
+        vol = inVol;
+    }
+    public void SetPitchDelta(float inDelta)
+    {
+        pitch = inDelta;
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
