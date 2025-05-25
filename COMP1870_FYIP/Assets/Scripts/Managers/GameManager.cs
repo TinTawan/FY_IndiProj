69eb31d57053ad2e12276b3dfbb5f0e6e1380238
Justin Ward <jw4566c@gre.ac.uk>
2025-05-25 19:02:22 +0100

    added circle screen fade when player wins
    UI circle expands when player gets 5 objectives

--- COMP1870_FYIP/Assets/Scripts/Managers/GameManager.cs
+++ COMP1870_FYIP/Assets/Scripts/Managers/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public int objectsPlaced = 0;
+    public bool win = false;
 
     private void Awake()
     {
@@ -17,15 +19,28 @@ public class GameManager : MonoBehaviour
     public void IncrementObjects()
     {
         objectsPlaced++;
+
+        if(objectsPlaced >= 5)
+        {
+            win = true;
+        }
     }
 
     private void Update()
     {
-        if(objectsPlaced >= 5)
+        if(win)
         {
             //win
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            StartCoroutine(WinCountDown());
         }
     }
 
+    IEnumerator WinCountDown()
+    {
+        win = false;
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
 }
